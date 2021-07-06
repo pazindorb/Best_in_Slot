442c847914221b732a45967605315781873ec061
pazindorb <56399197+pazindorb@users.noreply.github.com>
2021-07-06 15:13:24 +0200

    Pre-refactor version

--- src/main/java/pl/bloniarz/bis/model/dto/request/equipmentset/CharacterEquipmentSetRequest.java
+++ src/main/java/pl/bloniarz/bis/model/dto/request/equipmentset/CharacterEquipmentSetRequest.java
@@ -1,12 +1,14 @@
 package pl.bloniarz.bis.model.dto.request.equipmentset;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CharacterEquipmentSetRequest {
 
     private String name;
